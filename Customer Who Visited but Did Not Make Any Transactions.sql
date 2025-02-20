ae9fc443b453a2826cf59b384e2a5c3eb1413085
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-20 12:43:13 -0500

    Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT 
+  customer_id, 
+  COUNT(visit_id) AS count_no_trans 
+FROM 
+  Visits 
+WHERE 
+  visit_id NOT IN (
+    SELECT 
+      visit_id 
+    FROM 
+      Transactions
+  ) 
+GROUP BY 
+  customer_id
