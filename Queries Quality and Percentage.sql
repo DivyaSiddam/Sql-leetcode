988e3f8f90ac5a46aa53542d88bda8bb861283b1
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-27 23:34:54 -0500

    Queries Quality and Percentage.sql

---
+++ Queries Quality and Percentage.sql
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT 
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(100 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
